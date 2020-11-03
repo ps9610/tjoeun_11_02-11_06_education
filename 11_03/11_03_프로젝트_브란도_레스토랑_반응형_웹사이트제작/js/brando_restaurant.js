1c1dd53c5f842136163634ba5f6310f163dee8bd
ps9610 <ps961001@gmail.com>
2020-11-03 10:13:18 +0900

    11_03_브란도_프로젝트_이어서

---
+++ 11_03/11_03_프로젝트_브란도_레스토랑_반응형_웹사이트제작/js/brando_restaurant.js
@@ -0,0 +1,99 @@
+;(function(window,document,$,undefined){ 
+
+    var brando = {
+        
+        init:           function(){ 
+            var that=this;
+                that.headerFn();
+                that.section01Fn();
+                that.section02Fn();
+                that.section03Fn();
+                that.section04Fn();
+                that.section05Fn();
+                that.section06Fn();
+                that.section07Fn();
+                that.section08Fn();
+                that.section09Fn();
+                that.section10Fn();
+                that.section11Fn();
+                that.section12Fn();
+                that.section13Fn();
+                that.footerFn();
+        },//브란도 레스토랑 전체에서 최초에 실행할 js
+        headerFn:       function(){
+
+        },//헤더의 js
+        section01Fn:    function(){
+           
+            var winH = 969;
+            //뭘 먼저 해야할지 우선순위 정리가 가장 첫번째
+            //1. hungry 이미지 탑 값 구하기 = (window top - hungry높이)/2;            
+                                //ㄴ> imgTop = (winH-imgH)/2;
+            var imgH = $(".hungry").height();
+            var imgTop = (winH-imgH)/2;
+    
+            setTimeout(resizeFn,100);
+            function resizeFn(){                
+                winH = $(window).height();
+                $("#section01, #section02, #section03, #section04").css({ height:winH });
+                
+                imgH = $(".hungry").height();
+                imgTop = (winH-imgH)/2;
+                $(".hungry").css({ top:imgTop });
+            };
+            //Smooth Scrolling Event
+            $(".arrow-down-btn").on({
+                click : function(){
+                    $("html,body").stop().animate({ scrollTop : $("#section02").offset().top},700);
+                }
+            });
+
+            $(window).resize(function(){
+                resizeFn();
+            });
+        },
+        section02Fn:    function(){
+
+        },
+        section03Fn:    function(){
+
+        },
+        section04Fn:    function(){
+            
+        },
+        section05Fn:    function(){
+            
+        },
+        section06Fn:    function(){
+            
+        },
+        section07Fn:    function(){
+            
+        },
+        section08Fn:    function(){
+            
+        },
+        section09Fn:    function(){
+            
+        },
+        section10Fn:    function(){
+            
+        },
+        section11Fn:    function(){
+            
+        },
+        section12Fn:    function(){
+            
+        },
+        section13Fn:    function(){
+            
+        },
+        footerFn:         function(){
+            
+        }
+    }; 
+
+//위에서 함수를 만들고 밑에서 함수를 실행한다.
+    brando.init();
+
+})(window,document,jQuery);
