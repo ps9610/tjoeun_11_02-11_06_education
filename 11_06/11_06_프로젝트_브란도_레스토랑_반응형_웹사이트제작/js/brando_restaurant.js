b97d82966bf1ecfde3ad897b69ffb1e15f90b729
ps9610 <ps961001@gmail.com>
2020-11-06 10:29:16 +0900

    박스_블럭_초기화_후_다시_생성

--- 11_06/11_06_프로젝트_브란도_레스토랑_반응형_웹사이트제작/js/brando_restaurant.js
+++ 11_06/11_06_프로젝트_브란도_레스토랑_반응형_웹사이트제작/js/brando_restaurant.js
@@ -94,67 +94,7 @@
             });
         },
         section02Fn:    function(){
-            //해당 섹션의 폰트사이즈 비율
-            var txtBoxW = 0;
-            var fontRateH3 = 0.082758621;
-            var fontRateH4 = 0.037931034;
-            var fontRateP = 0.048275862;
-            var fontSizeH3 = 0;
-            var fontSizeH4 = 0;
-            var fontSizeP =  0;
-            var boxHRate = 1.2222222;
-            var boxW = 450;
-            var boxH = 0; 
-            var winH = 0;
-            var boxT = 0; 
-            var winW = 0;
-            var boxR = 0; 
-            var boxL = 0; 
-
-            setTimeout(resizeFn,100);
-
-            //해당 섹션의 폰트사이즈 비율
-            function resizeFn(){
-                
-            txtBoxW = $(".content-wrap").width(); 
-            boxW = $(".content").width(); 
-            boxH = boxW * boxHRate;
-            fontSizeH3 =  txtBoxW * fontRateH3;
-            fontSizeH4 =  txtBoxW * fontRateH4;
-            fontSizeP =  txtBoxW * fontRateP;
-            winH = $(window).height();
-            boxT = (winH-boxH)/2; 
-            winW = $(window).width();
-            boxR = (winW-boxW)/2; 
-            boxL = boxR 
-            //console.log ( "창넓이" ,winW );
-            //console.log ( "박스 넓이 ",boxW );
-            //console.log ( "박스 right값 ",boxR );
-            //console.log ( "박스 left값 ",boxL ); 
-
-            $(".content-wrap h3").css({fontSize:fontSizeH3});
-                $(".content-wrap h4").css({fontSize:fontSizeH4});
-                $(".content-wrap p").css({fontSize:fontSizeP});
-
-                $(".content").css({height:boxH,top:boxT});
-
-                //창 너비가 1170이하이면 실행
-                if(winW <= 1170){
-                    $(".content24").stop().animate({right:boxR,},300);
-                    $(".content3").stop().animate({left:boxL},300);
-                }
-                else{
-                    $(".content24").stop().animate({right:0,},300);
-                    $(".content3").stop().animate({left:0,},300);
-                }
-            }
-            // console.log( "텍스트박스 너비 ", txtBoxW, "글자크기비율", fontRateH3, "실제글자크기", fontSizeH3 );
-            // console.log( "텍스트박스 너비 ", txtBoxW, "글자크기비율", fontRateH4, "실제글자크기", fontSizeH4 );
-            // console.log( "텍스트박스 너비 ", txtBoxW, "글자크기비율", fontRateP, "실제글자크기", fontSizeP );
-
-            $(window).resize(function(){
-                resizeFn();
-            });
+        
         },
         section03Fn:    function(){
 
