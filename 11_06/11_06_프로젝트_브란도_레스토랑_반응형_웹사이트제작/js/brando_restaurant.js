33ac9d52fe4ac9d913790608dcc42b10cad348e9
ps9610 <ps961001@gmail.com>
2020-11-06 14:42:35 +0900

    프로젝트_브란도_반응형_박스블럭_제이쿼리_완료

--- 11_06/11_06_프로젝트_브란도_레스토랑_반응형_웹사이트제작/js/brando_restaurant.js
+++ 11_06/11_06_프로젝트_브란도_레스토랑_반응형_웹사이트제작/js/brando_restaurant.js
@@ -5,9 +5,13 @@
             var that=this;
                 that.headerFn();
                 that.section01Fn();
-                that.section02Fn();
-                that.section03Fn();
-                that.section04Fn();
+                that.section234Fn();
+                /* 
+                that.section234Fn(); 
+                 =  that.section02Fn();
+                    that.section03Fn();
+                    that.section04Fn();
+                */
                 that.section05Fn();
                 that.section06Fn();
                 that.section07Fn();
@@ -75,7 +79,7 @@
             setTimeout(resizeFn,100);
             function resizeFn(){                
                 winH = $(window).height();
-                $("#section01, #section02, #section03, #section04").css({ height:winH });
+                $("#section01").css({ height:winH });
                 
                 imgH = $(".hungry").height();
                 imgTop = (winH-imgH)/2;
@@ -93,15 +97,71 @@
                 resizeFn();
             });
         },
-        section02Fn:    function(){
-        
-        },
-        section03Fn:    function(){
+        section234Fn:    function(){
+         
+            var rl = (windowWidth-boxWidth)/2;
+            var windowWidth = $(window).width(); //1170
+            var windowHeight = $(window).height(); //969
+            var section234Height = windowHeight; 
+            var boxTop = (windowHeight-boxHeight)/2; //(969-550)/2 = 209.5
+            var boxWidth = $(".content-wrap").width(); //450
+            var boxHeight = boxWidth * 1.22222;
+            //            = $(".content-wrap").height(); //550
 
-        },
-        section04Fn:    function(){
+//박스 넓이가 바뀌면 height도 바뀌여야 함 이거 할거임
+//박스 높이(고정되어있음) = 박스 너비 (450)* 높이의 비율 = 1.22222
+// boxHeight = boxWidth * 1.22222
+// boxWidth는 boxHeight 위 쪽으로 변수 써주기
+
+//창 너비가 1170이하면 [조건문] 박스를 가운데 정렬하는 애니메이션 만들기
+// right(또는 left) = (창너비-박스너비)/2
+// 창 너비 변수 생성하기
+// 그리고 right(또는 left) 변수 생성하기
+            setTimeout(resizeFn,100);
+
+            function resizeFn(){
+                
+                rl = (windowWidth-boxWidth)/2;
+                windowWidth = $(window).width(); //1170
+                windowHeight = $(window).height(); //969
+                section234Height = windowHeight;
+                boxWidth = $(".content-wrap").width(); //450
+                boxHeight = boxWidth * 1.22222;
+                // boxTop = (windowHeight-boxHeight)/2; //(969-550)/2 = 209.5
+
+                if(windowHeight < boxHeight){
+                    section234Height = boxHeight;
+                    boxTop = 0;
+                }
+                else{
+                    section234Height = windowHeight;
+                    boxTop = (windowHeight-boxHeight)/2;
+                }
+            console.log( boxHeight );
+            $(".content-wrap").css({ top:boxTop, height:boxHeight });
+            $(".section234").css({ height:section234Height });
             
+                if( windowWidth <= 1170 ){
+                    $("#section02 .content-wrap, #section04 .content-wrap").stop().animate({ right:rl-15 },300);
+                    // $("#section02 .content-wrap, #section04 .content-wrap").css({ right:rl-15 }) : 애니메이션 하기 전
+                    // $("#section02 .content-wrap, #section04 .content-wrap").css({ right:rl-15 = 마진값 빼줘야 한쪽으로 안 치우치고 중앙에 옴});
+                    $("#section03 .content-wrap").stop().animate({ left:rl-15 },300);
+                }
+                else{
+                    $("#section02 .content-wrap, #section04 .content-wrap").stop().animate({ right:0 },100);
+                    $("#section03 .content-wrap").stop().animate({ left:0 },100);
+                }
+            };
+
+            $(window).resize(function(){
+                resizeFn();
+            })
         },
+/*        section234Fn:    function(){} =
+                section02Fn:function(){},
+                section03Fn:function(){},
+                section04Fn:function(){},
+*/
         section05Fn:    function(){
             
         },
